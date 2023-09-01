723e0d43aae5c92bbf5d1342fad43fa77119017c
sahil-kaler <119391867+sahil-kaler@users.noreply.github.com>
2023-09-01 22:14:54 +0530

    commit

--- app.js
+++ app.js
@@ -5,12 +5,19 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));  
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
+app.get("/",function(req,res){
+    res.sendFile( __dirname + "/signup.html");
+});
 
+app.post("/",function(req,res){
+    var firstName = req.body.fName;
+    var secondName = req.body.lName;
+    var email = req.body.email;
+})
 
-
-
-app.listen(3000, function(){
-    console.log("Server is running on port 3000");
+app.listen(5000, function(){
+    console.log("Server is running on port 5000");
 });
